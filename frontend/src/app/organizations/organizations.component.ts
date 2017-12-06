147b6199170ed82e495e68c1099d75652c8471b2
Simon <szmiekus@gmail.com>
2017-12-06 17:53:17 +0100

    Add implementation organizations.service and use it in organizations.component

--- frontend/src/app/organizations/organizations.component.ts
+++ frontend/src/app/organizations/organizations.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OrganizationsService } from './organizations.service';
+import { Organization } from '../organization/organization.model';
 
 @Component({
   selector: 'app-organizations',
   templateUrl: './organizations.component.html',
-  styleUrls: ['./organizations.component.css']
+  styleUrls: ['./organizations.component.css'],
+  providers: [OrganizationsService]
 })
 export class OrganizationsComponent implements OnInit {
+  organizations: Array<Organization> = [];
 
-  constructor() { }
+  constructor(private organizationsService: OrganizationsService) { }
 
   ngOnInit() {
+    this.organizationsService.getOrganizations()
+      .subscribe(
+        organizations => {
+          this.organizations = organizations;
+        }
+      );
   }
-
 }
--- frontend/src/app/organizations/organizations.service.ts
+++ frontend/src/app/organizations/organizations.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class OrganizationsService {
+  private url = `${environment.apiRoot}/organizations/`
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  getOrganizations() {
+    return this.http.get(this.url, { withCredentials: true } )
+      .map((res: Response) => res.json())
+  }
 }
